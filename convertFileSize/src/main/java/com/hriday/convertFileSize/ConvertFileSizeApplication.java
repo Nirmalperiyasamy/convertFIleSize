2c5020e6902065189c6cebc28fda3981b202d94c
Nirmal <105903253+Nirmalperiasamy@users.noreply.github.com>
2023-03-27 15:23:17 +0530

    Initial commit

---
+++ convertFileSize/src/main/java/com/hriday/convertFileSize/ConvertFileSizeApplication.java
@@ -0,0 +1,13 @@
+package com.hriday.convertFileSize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertFileSizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertFileSizeApplication.class, args);
+	}
+
+}
